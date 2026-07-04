bdd48320325fc0ce5deb1a4af4f1c9827369474f
RUYANGA <ruyangam15@gmail.com>
2026-07-04 17:35:53 +0200

    Add Borrower model with serialization support

---
+++ Module1/LAB2/Library-Inventory/src/library_inventory/borrower.py
@@ -0,0 +1,45 @@
+class Borrower:
+    """Represents a library patron who can borrow books."""
+
+    def __init__(self, id, name, email):
+        self._id = id
+        self._name = name
+        self._email = email
+
+    @property
+    def id(self):
+        """Unique identifier for the borrower."""
+        return self._id
+
+    @property
+    def name(self):
+        """Full name of the borrower."""
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = value
+
+    @property
+    def email(self):
+        """Email address of the borrower."""
+        return self._email
+
+    @email.setter
+    def email(self, value):
+        self._email = value
+
+    def to_dict(self):
+        """Serialize borrower to a dictionary."""
+        return {"id": self._id, "name": self._name, "email": self._email}
+
+    @staticmethod
+    def from_dict(data):
+        """Create a Borrower instance from a dictionary."""
+        return Borrower(data["id"], data["name"], data["email"])
+
+    def __repr__(self):
+        return f"Borrower({self._id}, '{self._name}')"
+
+    def __eq__(self, other):
+        return isinstance(other, Borrower) and self._id == other._id
