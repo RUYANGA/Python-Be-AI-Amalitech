b4515f2446bb1673767e3438e92b72a10b3fef0e
RUYANGA <ruyangam15@gmail.com>
2026-08-27 12:46:57 +0200

    Remove GET /api/v1/urls/ list endpoint; keep POST create and /urls/mine/ list

--- Module6/src/apps/shortener/api/urls.py
+++ Module6/src/apps/shortener/api/urls.py
@@ -13,13 +13,13 @@ from django.urls import path
 
 from apps.shortener.api.views import (
     URLAnalyticsByCodeView,
-    URLCollectionView,
+    URLCreateView,
     URLListView,
     URLShortCodeDetailView,
 )
 
 urlpatterns = [
-    path("urls/", URLCollectionView.as_view(), name="url-collection"),
+    path("urls/", URLCreateView.as_view(), name="url-create"),
     path("urls/mine/", URLListView.as_view(), name="url-list-mine"),
     path("urls/<str:short_code>/", URLShortCodeDetailView.as_view(), name="url-by-code"),
     path(
--- Module6/src/apps/shortener/api/views/__init__.py
+++ Module6/src/apps/shortener/api/views/__init__.py
@@ -1,7 +1,6 @@
 """HTTP views for the shortener API endpoints."""
 
 from apps.shortener.api.views.analytics_by_code_view import URLAnalyticsByCodeView
-from apps.shortener.api.views.collection_view import URLCollectionView
 from apps.shortener.api.views.create_view import URLCreateView
 from apps.shortener.api.views.list_view import URLListView
 from apps.shortener.api.views.resolve_view import URLResolveView
@@ -9,7 +8,6 @@ from apps.shortener.api.views.short_code_view import URLShortCodeDetailView
 
 __all__ = [
     "URLAnalyticsByCodeView",
-    "URLCollectionView",
     "URLCreateView",
     "URLListView",
     "URLResolveView",
